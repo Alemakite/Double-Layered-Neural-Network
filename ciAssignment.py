47496fd7db0264743cff16e577fbca134717c8d1
Piotr Kosek <serpier26@gmail.com>
2022-04-29 19:18:44 +0200

    Add files via upload

--- ciAssignment.py
+++ ciAssignment.py
@@ -26,14 +26,14 @@ testInputs2 = []
 testOutputs = []
 
 for x in training:
-    trainingInputs1.append(x[0])
-    trainingInputs2.append(x[1])
-    trainingOutputs.append(x[2])
+    trainingInputs1.append(float(x[0]))
+    trainingInputs2.append(float(x[1]))
+    trainingOutputs.append(float(x[2]))
 
 for x in test:
-    testInputs1.append(x[0])
-    testInputs2.append(x[1])
-    testOutputs.append(x[2])
+    testInputs1.append(float(x[0]))
+    testInputs2.append(float(x[1]))
+    testOutputs.append(float(x[2]))
 
 # weigths for inputs 
 i1weights = [0, 0]
@@ -51,67 +51,85 @@ h2weight = random.random()
 #data from the test set, then study performance (output of test)
 
 # set the learning rate
-learningRate = 0.7
+learningRate = 0.5
 
-# array to store network outputs
-networkOutput = []
+# list to store network outputs
+networkOutputs = [0 for i in range (0,45)]
 
 # array to store errors
-errors = []
+errors = [0 for i in range (0,45)]
 
 hasConvergence = False
 
 euler = 2.7182
 
-sum1 = 0
-sum2 = 0
-
-
 def noErrors(listOfErrors):
-    convergence = False
+    noOfErrors = 0
     #iterate through error list
     #if there's at least one error, break the loop and return false later
     for x in listOfErrors:
-        if x < 0.5 and x > -0.5:
-            convergence = True
-        else:
-            convergence = False
-            break
-    if convergence == True:
+        if x > 0.5 or x < -0.5:
+            noOfErrors += 1
+            
+    print(noOfErrors)
+    if noOfErrors == 0:
         return True
     else:
         return False
 
-
+#TRAINING
 while hasConvergence == False:
-    for i in range(0, len(trainingInputs1)):
+#for epoch in range (0, 200):
+        #for i in range(0, len(training)):
+    for i1,i2,target,Y,e in zip(trainingInputs1, trainingInputs2,
+                                     trainingOutputs, networkOutputs,
+                                     errors):
     #Forthpropagation
-        
+        '''
         i1 = float(trainingInputs1[i])
         i2 = float(trainingInputs2[i])
-        out = float(trainingOutputs[i])
+        target = float(trainingOutputs[i])
+        '''
+        
+        f = i1 * i1weights[0]
+        g = i2 * i2weights[0]
+        k = i1 * i1weights[1]
+        l = i2 * i2weights[1]
 
-        sum1 = (i1 * i1weights[0]) + (i2 * i2weights[0])
-        sum2 = (i1 * i1weights[1]) + (i2 * i2weights[1])
-            
+        sum1 = f + g
+        sum2 = k + l
         #sigmoid transfer function - transform outputs of layer 1 into inputs of layer 2
         h1 = 1/(1 + euler **(-sum1))
         h2 = 1/(1 + euler **(-sum2))
-        
-        networkOutput.insert(i, ((h1 * h1weight) + (h2 * h2weight)))
-        errors.insert(i, learningRate * (out - networkOutput[i]))
-    
+        '''
+        networkOutput[i] = (h1 * h1weight) + (h2 * h2weight)
+        errors[i] = learningRate * (target - networkOutput[i])
+        '''
+        Y = (h1 * h1weight) + (h2 * h2weight)
+        e = target - Y
     #Backpropagation
         
         #Hidden layer weights update:
         #how much error changes with respect to output
-        a = -float(errors[i])
+        '''
+        a = -1 * (target - networkOutput[i])
+        '''
+        a = -1 * (target - Y)
         #how much final output changes with respect to the total inputs
-        b = float(networkOutput[i] * (1 - networkOutput[i]))
+        '''
+        b = networkOutput[i] * (1 - networkOutput[i])
+        '''
+        b = Y * (1 - Y)
         #how much total input of the net changes with respect to hidden neuron1's weight
-        c = float(networkOutput[i] * h1weight)
+        '''
+        c = 1 * networkOutput[i] * h1weight
+        '''
+        c = 1 * Y * h1weight
         #how much total input of the net changes with respect to hidden neuron2's weight
-        d = float(networkOutput[i] * h2weight)
+        '''
+        d = 1 * networkOutput[i] * h2weight
+        '''
+        d = 1 * Y * h2weight
         h1weightUpdate = a * b * c
         h2weightUpdate = a * b * d
         h1weight = h1weight - h1weightUpdate
@@ -120,30 +138,28 @@ while hasConvergence == False:
         #Layer1 weights update:
         #Related to h1
         #how much Y changes with respect to h1
-        e = float(1 - h1)
-        e2 = float(h1 * e)
-        #total net input to h1 with respect to input1's weight1
-        f = i1 * i1weights[0]
-        #total net input to h1 with respect to input2's weight1
-        g = i2 * i2weights[0]
-        i1weightUpdate1 = a * b * e * f
-        i2weightUpdate1 = a * b * e * g
-        i1weights[0] = i1weights[0] - i1weightUpdate1
-        i2weights[0] = i2weights[0] - i2weightUpdate1
+        '''
+        m = float(h1 * (networkOutput[i] - h1))
+        '''
+        m = h1 * (Y - h1)
+        i1weightUpdate0 = a * b * m * f
+        i2weightUpdate0 = a * b * m * g
+        i1weights[0] = i1weights[0] - i1weightUpdate0
+        i2weights[0] = i2weights[0] - i2weightUpdate0
         #Related to h2
         #how much Y changes with respect to h2
-        j = float(1 - h2)
-        j2 = float(h2 * j)
-        #total net input to h1 with respect to input1's weight2
-        k = i1 * i1weights[1]
-        #total net input to h1 with respect to input2's weight2
-        l = i2 * i2weights[1]
-        i1weightUpdate2 = a * b * j2 * k
-        i2weightUpdate2 = a * b * j2 * l
-        i1weights[1] = i1weights[1] - i1weightUpdate2
-        i2weights[1] = i2weights[1] - i2weightUpdate2
-        print(errors[i])
+        '''
+        j = float(h2 * (networkOutput[i] - h2))
+        '''
+        j = h2 * (Y - h2)
+        i1weightUpdate1 = a * b * j * k
+        i2weightUpdate1 = a * b * j * l
+        i1weights[1] = i1weights[1] - i1weightUpdate1
+        i2weights[1] = i2weights[1] - i2weightUpdate1
+        #print(errors[i])
+        print(i2weights)
     hasConvergence = noErrors(errors)
 
 if hasConvergence == True:
-        print("Achieved convergence.")
+        print("Training achieved convergence.")
+        print("Presenting test set.")
